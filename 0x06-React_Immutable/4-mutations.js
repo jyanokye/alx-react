3d55ee2b1424012f15f2b1d56b267babd207488b
jyanokye <101642075+jyanokye@users.noreply.github.com>
2023-05-13 02:39:55 +0000

    Update 4-mutations.js

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,14 +1,14 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
   4: 'Oliver',
   5: 'Jacob',
-  6: 'Lucas'
+  6: 'Lucas',
 });
 
-const map2 = map.set('2', 'Benjamin').set('4', 'Oliver');
-
-export { map, map2 };
+export const map2 = map.withMutations((values) => {
+  values.set(2, 'Benjamin').set(4, 'Oliver');
+});
